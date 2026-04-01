6c1e8e7330fe757ec52a78ce01049cbcd535d189
Sahithi Gorantla <25b0926@iitb.ac.in>
2026-04-01 18:06:16 +0530

    Refactor user registration and password handling
    Refactored user registration and password checking functions. Improved password hashing and output formatting.

--- main.sh
+++ main.sh
@@ -1,6 +1,37 @@
-register_user(){
+#!/bin/bash
+register_user(){ 
 read -p "Enter Username:" username
-read -sp "Enter Password:" password
-hashed=$(echo -n "$password" | sha256sum | awk "$1")
-echo "$username-$hashed" > users.tsv
+
+read -p "Enter Password:" password
+hashed=$(echo -n "$password" | sha256sum | awk '{print$1}')
+echo -e "$username\t$hashed" >> users.tsv
 }
+#check_password checks if the password is correct 
+check_password() {
+    username=$1
+
+    while true; do
+        read -p "Enter password: " password
+        echo ""
+
+        hashed=$(echo -n "$password" | sha256sum | awk '{print $1}')
+
+        stored_hash=$(grep "^$username	" users.tsv | awk '{print $2}')
+
+        if [ "$hashed" == "$stored_hash" ]; then
+            echo "Login Successful!!"
+            return
+        else
+            echo "Incorrect password, please re-enter."
+        fi
+    done
+}
+
+
+# Checking if they are different users 
+while [ "$player1" = "$player2" ]; do
+    echo "Both players cannot be same. Enter Player 2 again."
+    player2=$(authenticate)
+done
+python3 game.py "$player1" "$player2"
+ 
